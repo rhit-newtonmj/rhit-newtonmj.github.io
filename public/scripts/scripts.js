64da6ba6336bee2720a9149541d9bcff8af4e5a2
Megan Newton <newtonmj@rose-hulman.edu>
2025-09-26 22:16:15 -0400

    Added footer

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,5 @@
 const headerLinks = {Home:"index.html", Resume:"./resume.html", Portfolio:"./portfolio.html"};
-const footerLinks = {};
+const footerLinks = {Handshake:"https://rose-hulman.joinhandshake.com/profiles/6ns8uu"};
 
 function addHeader(){
     let header = document.querySelector("header");
@@ -8,6 +8,17 @@ function addHeader(){
         link.innerText = page;
         link.href = headerLinks[page];
         header.appendChild(link);
+    });
+
+    }
+
+function addFooter(){
+    let footer = document.querySelector("footer");
+    Object.keys(footerLinks).forEach((page) =>{
+        let link = document.createElement("a");
+        link.innerText = page;
+        link.href = footerLinks[page];
+        footer.appendChild(link);
     });
 
     }
